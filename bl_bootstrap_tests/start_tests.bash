4cbf405681393b46ccd8be8ee4a3e87782221ac4
vialuk <Viacheslav.Lukashenkov@krontech.ru>
2018-08-09 16:47:27 +0300

    linux scripts and conf

--- bl_bootstrap_tests/start_tests.bash
+++ bl_bootstrap_tests/start_tests.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
+export PYTHONPATH=/home/slon/BL_tests_project/bl_frame_work 
 cd /home/slon/BL_tests_project/bl_frame_work/test_bl/test_sonata_plugin/tests_sonata
 python3.6 test_loader_sonata.py
+
