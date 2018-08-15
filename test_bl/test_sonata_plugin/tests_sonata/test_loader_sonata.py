e243709316539bcd9ce0a236a255153c678b42a4
vialuk <Viacheslav.Lukashenkov@krontech.ru>
2018-08-15 16:23:50 +0300

    Fix hardcoded path for tests loader

--- test_bl/test_sonata_plugin/tests_sonata/test_loader_sonata.py
+++ test_bl/test_sonata_plugin/tests_sonata/test_loader_sonata.py
@@ -1,15 +1,28 @@
-import unittest, time, xmlrunner, datetime
+import unittest, time, xmlrunner, datetime, platform, os
 
 
 
 if __name__ == '__main__':
+        '''Lets find ot the system we run on'''
+        syst = platform.system()
+        '''And where we are'''
+        proj_abs_path = os.path.abspath(os.path.dirname(__file__))
+        start_dir = proj_abs_path
 
+        '''
+        if syst == 'Windows':
+                start_dir = os.path.join(proj_abs_path,
+                                        "tests_sonata")
+        elif syst == 'Linux':
+                start_dir = os.path.join(proj_abs_path,
+                                         "tests_sonata")
+        '''
         counter = 0
         date_now = datetime.datetime.now()
         date_now_str = date_now.strftime("%Y-%m-%d_%H-%M")
         loader = unittest.TestLoader()
         #C:\data\kronshtadt\QA\BL\AutomationFrameworkDesign\bl_frame_work\test_bl\test_sonata_plugin\tests_sonata
-        start_dir = 'C:\\data\\kronshtadt\\QA\\BL\\AutomationFrameworkDesign\\bl_frame_work\\test_bl\\test_sonata_plugin\\tests_sonata'
+        #start_dir = 'C:\\data\\kronshtadt\\QA\\BL\\AutomationFrameworkDesign\\bl_frame_work\\test_bl\\test_sonata_plugin\\tests_sonata'
         suite = loader.discover(start_dir)
         #runner = xmlrunner.XMLTestRunner(output='./sonata-xml-test-reports-'+str(counter)+' '+date_now_str)
         runner = xmlrunner.XMLTestRunner(output='C:/data/kronshtadt/QA/BL/Test_Logs/sonata-xml-test-reports-' + str(counter) + ' ' + date_now_str)
